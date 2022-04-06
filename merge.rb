1958719d0fc4c757ffb48f26d904d52e0d9fd3f0
supersakana <zacwilliamson@icloud.com>
2022-04-06 09:15:42 -0600

    different merge sortmethod

--- merge.rb
+++ merge.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-'https://www.honeybadger.io/blog/ruby-merge-sort/'
-
 # What is a merge sort?
 # Sorts smaller arrays and merge them in sorted order.
 # Levereages a recursion.
@@ -12,34 +10,25 @@
 # Merge the 2 halfs together.
 
 # The goal of this method is to split the array into halfs until there are only single elements
-def sort(numbers)
-  num_elements = numbers.length
-  return numbers if num_elements <= 1
-
-  half_of_elements = (num_elements / 2).round
-
-  left  = numbers.take(half_of_elements)
-  right = numbers.drop(half_of_elements)
-
-  sorted_left = sort(left)
-  sorted_right = sort(right)
-
-  merge(sorted_left, sorted_right)
+def merge_sort(array)
+  if array.length <= 1
+    array
+  else
+    mid = (list.length / 2).floor
+    left = merge_sort(array[0..mid - 1])
+    right = merge_sort(array[mid..list.length])
+    merge(left, right)
+  end
 end
 
-def merge(left_array, right_array)
-  return left_array if right_array.empty?
-
-  return right_array if left_array.empty?
-
-  smallest_number = if left_array.first <= right_array.first
-                      left_array.shift
-                    else
-                      right_array.shift
-                    end
-
-  recursive = merge(left_array, right_array)
-
-  [smallest_number].concat(recursive)
+def merge(left, right)
+  if left.empty?
+    right
+  elsif right.empty?
+    left
+  elsif left.first < right.first
+    [left.first] + merge(left[1..left.length], right)
+  else
+    [right.first] + merge(left, right[1..right.length])
+  end
 end
-sort([5, 2, 1, 3, 6, 4, 8])
